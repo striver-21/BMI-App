0046e6bd48c48157d23f7d021fac91bee18d9b4a
striver-21 <sachinsoni722@gmail.com>
2021-12-06 18:30:38 +0530

    Files added

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bmi/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
